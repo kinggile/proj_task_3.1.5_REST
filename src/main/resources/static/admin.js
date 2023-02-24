fb9d900dae278035a67d8c5687aa033ff7ffeca2
Yegor <edeykin85@gmail.com>
2023-02-24 22:20:07 +0300

    addNewUser works properly

--- src/main/resources/static/admin.js
+++ src/main/resources/static/admin.js
@@ -1,19 +1,32 @@
 $(async function () {
     await getTableWithAllUsers();
+    await addUser()
 })
 
 
 const fetchService = {
     head: {
-        "Accept-Type": "application/json",
+        // "Accept-Type": "application/json",
         "Content-Type": "application/json"
     },
 
     findAllUsers: async () => await fetch("http://localhost:8080/rest"),
+
     addNewUser: async (user) => await fetch("http://localhost:8080/rest/new", {
         method: "POST",
         headers: fetchService.head,
         body: JSON.stringify(user)
+    }),
+
+    updateUser: async (user, id) => await fetch(`http://localhost:8080/rest/edit/${id}`, {
+        method: "PATCH",
+        headers: fetchService.head,
+        body: JSON.stringify(user)
+    }),
+
+    deleteUser: async (id) => await fetch(`http://localhost:8080/rest/delete/${id}`, {
+        method: "DELETE",
+        // headers: fetchService.head
     })
 }
 
@@ -35,13 +48,14 @@ async function getTableWithAllUsers() {
                             <td>${user.roles.map(r => r.role.substring(5))}</td>
                             
                             <td>
-                                 <button type="button" class="btn btn-info" style="color: white" data-bs-toggle="modal">
+                                 <button type="button" id="editBtn" class="btn btn-info" data-bs-toggle="modal"
+                                 style="color: white">
                                                 Edit
                                  </button>
                             </td>
                             
                               <td>
-                                  <button type="button" class="btn btn-danger" data-bs-toggle="modal">
+                                  <button type="button" id="deleteBtn" class="btn btn-danger" data-bs-toggle="modal">
                                                 Delete
                                   </button>
                               </td>
@@ -54,6 +68,58 @@ async function getTableWithAllUsers() {
         .catch(err => console.log(err))
 }
 
+
+
+
+let roleList = [
+    {id: 1, role: "ROLE_ADMIN"},
+    {id: 2, role: "ROLE_USER"}
+]
+
+let newUserForm = document.forms["addNewUserForm"]
+
+
+async function addUser() {
+    newUserForm.addEventListener("submit", (ev) => {
+        ev.preventDefault()
+
+        let checkRole = () => {
+            let roles = []
+            let option = document.querySelector("#role").options
+
+            for (let i = 0; i < option.length; i++) {
+                if (option[i].selected) {
+                    roles.push(roleList[i])
+                }
+            }
+
+            return roles;
+        }
+
+        fetch("http://localhost:8080/rest/new", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+
+            body: JSON.stringify({
+                id: newUserForm.id.value,
+                firstname: newUserForm.firstname.value,
+                lastname: newUserForm.lastname.value,
+                age: newUserForm.age.value,
+                username: newUserForm.username.value,
+                password: newUserForm.password.value,
+                roles: checkRole()
+            })
+        })
+
+            .then(() => {
+                newUserForm.reset()
+                getTableWithAllUsers();
+                $("#test").click()
+            })
+    })
+}
 
 
 // window.onload = function() {
