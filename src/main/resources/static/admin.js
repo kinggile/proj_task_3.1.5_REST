543b9565bab8387b7a1565c44bb009e42134b8c0
Yegor <edeykin85@gmail.com>
2023-02-24 22:58:11 +0300

    package refactoring

--- src/main/resources/static/admin.js
+++ src/main/resources/static/admin.js
@@ -1,7 +1,12 @@
 $(async function () {
     await getTableWithAllUsers();
-    await addUser()
 })
+
+
+let roleList = [
+    {id: 1, role: "ROLE_ADMIN"},
+    {id: 2, role: "ROLE_USER"}
+]
 
 
 const fetchService = {
@@ -69,57 +74,6 @@ async function getTableWithAllUsers() {
 }
 
 
-
-
-let roleList = [
-    {id: 1, role: "ROLE_ADMIN"},
-    {id: 2, role: "ROLE_USER"}
-]
-
-let newUserForm = document.forms["addNewUserForm"]
-
-
-async function addUser() {
-    newUserForm.addEventListener("submit", (ev) => {
-        ev.preventDefault()
-
-        let checkRole = () => {
-            let roles = []
-            let option = document.querySelector("#role").options
-
-            for (let i = 0; i < option.length; i++) {
-                if (option[i].selected) {
-                    roles.push(roleList[i])
-                }
-            }
-
-            return roles;
-        }
-
-        fetch("http://localhost:8080/rest/new", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-
-            body: JSON.stringify({
-                id: newUserForm.id.value,
-                firstname: newUserForm.firstname.value,
-                lastname: newUserForm.lastname.value,
-                age: newUserForm.age.value,
-                username: newUserForm.username.value,
-                password: newUserForm.password.value,
-                roles: checkRole()
-            })
-        })
-
-            .then(() => {
-                newUserForm.reset()
-                getTableWithAllUsers();
-                $("#test").click()
-            })
-    })
-}
 
 
 // window.onload = function() {
