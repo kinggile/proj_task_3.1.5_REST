f66d3c66d64ccbc8df3f4691f84f19d076529867
Yegor <edeykin85@gmail.com>
2023-02-26 17:39:05 +0300

    delete useless code

--- src/main/resources/static/deleteModal.js
+++ src/main/resources/static/deleteModal.js
@@ -13,17 +13,7 @@ async function deleteModalData(id) {
 async function deleteUser() {
     deleteForm.addEventListener('submit', e => {
         e.preventDefault()
-
-        switch (deleteForm.roles.value) {
-            case '1' :
-                deleteForm.roles.value = 'ADMIN'
-                break;
-
-            case '2' :
-                deleteForm.roles.value = 'USER'
-                break;
-        }
-
+        
         fetch('http://localhost:8080/rest/delete/' + deleteForm.id.value, {
             method: 'DELETE',
             headers: {
